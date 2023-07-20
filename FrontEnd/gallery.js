6ec91450f338685801d9690b8955bf2cc86289bf
TheKingRoro <peyroutet.aurelie@gmail.com>
2023-07-21 01:13:02 +0200

    Login non terminé

--- FrontEnd/gallery.js
+++ FrontEnd/gallery.js
@@ -1,22 +1,48 @@
+//LOGIN
 
-export function ajoutListenerAddProjet() {
-   const formulaireProjet = document.querySelector(".formulaire-avis");
-   formulaireAvis.addEventListener("submit", function (event) {
-   event.preventDefault();
-   // Création de l’objet du nouvel avis.
-   const avis = {
-       pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
-       utilisateur: event.target.querySelector("[name=utilisateur]").value,
-       commentaire: event.target.querySelector("[name=commentaire]").value,
-       nbEtoiles: parseInt(event.target.querySelector("[name=nbEtoiles]").value)
-   };
-   // Création de la charge utile au format JSON
-   const chargeUtile = JSON.stringify(avis);
-   // Appel de la fonction fetch avec toutes les informations nécessaires
-   fetch("http://localhost:8081/avis", {
-       method: "POST",
-       headers: { "Content-Type": "application/json" },
-       body: chargeUtile
-   });
-   });
-   
+function loginuser() {
+  const formulaireLogin = document.querySelector(".connect");
+  formulaireLogin.addEventListener("submit", function (event) {
+    alert("script en cours");
+    event.preventDefault();
+    //recupération du couple email, password
+    const coupleLogin = {
+      email: event.target.querySelector("#email").value,
+      password: event.target.querySelector("#password").value,
+    };
+    console.log(coupleLogin);
+    //adaptation de la charge pour envoi
+    const chargeUtile = JSON.stringify(coupleLogin);
+
+    //appel de la fonction fetch avec la charge adaptée
+    fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers : {"accept" : "application/json"},
+      headers: {"Content-Type": "application/json"},
+      body: chargeUtile,
+    });
+  });
+}
+//appel de la fonction
+loginuser();
+console.log("chargé");
+
+/*document.getElementById("form").addEventListener("submit", auth);
+
+function auth(event) {
+  event.preventDefault();
+
+  const emailLog = document.getElementById("username").value;
+  const passwordLog = document.getElementById("password").value;
+  console.log(emailLog);
+
+  if (emailLog === email.value && passwordLog === password.value) {
+    window.location.replace("index.html");
+  }
+  if (emailLog === "" && passwordLog === "") {
+    alert("Please enter information");
+  } else {
+    alert("Please enter valid information");
+    return;
+  }
+}*/
--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,16 +1,19 @@
-//WORKS & Galeries
+//WORKS pour GALERIE et MODAL GALEIRE
 const reponseWorks = await fetch("http://localhost:5678/api/works");
 const works = await reponseWorks.json();
-//console.log(works);
-
+console.log(works);
+const reponseCategories = await fetch("http://localhost:5678/api/categories");
+const categories = await reponseCategories.json();
+//GALERIE
 function genererWorks(works) {
   for (let j = 0; j < works.length; j++) {
-    //const work = works[j];
-    //console.log(work);
+    const figure = works[j];
+    console.log(figure);
     const sectionWork = document.querySelector(".gallery");
     const elementWork = document.createElement("figure");
-    elementWork.setAttribute("id", `${works[j].categoryId}`);
-    //console.log(idWork);
+    elementWork.classList.add("galleryFigure");
+    const idWork = elementWork.setAttribute("id", `${works[j].categoryId}`);
+    console.log(idWork);
     const imageWork = document.createElement("img");
     imageWork.src = works[j].imageUrl;
     imageWork.alt = works[j].title;
@@ -21,83 +24,62 @@ function genererWorks(works) {
     elementWork.appendChild(titleWork);
   }
 }
-
 genererWorks(works);
 
-//HABILLAGE GALLERIE
+//CATEGORIES
 
-function genererWorksGallery(works) {
-  //traitement dans modal-container
-  const sectionWorkModal = document.querySelector(".modal-container");
-
-  for (let z = 0; z < works.length; z++) {
-    const workModal = works[z];
-    //console.log(workModal);
-
-    const elementWorkModal = document.createElement("figure");
-    //Id
-    const idWorkModal = elementWorkModal.setAttribute(
-      "id",
-      `${works[z].categoryId}`
-    );
-    console.log(idWorkModal);
-    //title
-    const imageWorkModal = document.createElement("img");
-    imageWorkModal.src = works[z].imageUrl;
-    imageWorkModal.alt = works[z].title;
-    const titleWorkModal = document.createElement("figcaption");
-    titleWorkModal.textContent = works[z].title;
-    //categoryId
-    //userId
-    //category: id + name
-    sectionWorkModal.appendChild(elementWorkModal);
-    elementWorkModal.appendChild(imageWorkModal);
-    elementWorkModal.appendChild(titleWorkModal);
-  }
-  //Ajout class css gallery-modal
-  sectionWorkModal.classList.add("gallery-modal");
-
-  //Création bouton
-  const buttonWork = document.querySelector(".submit");
-  const addButton = document.createElement("button");
-  addButton.classList.add(`button-add`);
-  addButton.textContent = "Ajouter une photo";
-  buttonWork.appendChild(addButton);
-}
-
-document.querySelector("#project-btn").addEventListener("click", () => {
-  //nettoyage de modal container
-  document.querySelector(".modal-container").innerHTML("");
-  genererWorksGallery(works);
-});
-
-//CATEGORIES & Filtres
-const reponseCategories = await fetch("http://localhost:5678/api/categories");
-const categories = await reponseCategories.json();
-console.log(categories);
 for (let i = 0; i < categories.length; i++) {
-  const categorie = categories[i];
+  const categoriebtn = categories[i];
   const sectionCategorie = document.querySelector(".filtres");
   const btnCategorie = document.createElement("button");
+  btnCategorie.setAttribute("id", "btnFiltre" + [i + 1]);
   btnCategorie.classList += "filtre";
-  btnCategorie.innerText = categorie.name;
+  btnCategorie.innerText = categoriebtn.name;
   sectionCategorie.appendChild(btnCategorie);
 }
 
-//const { log } = require("console");
-//const { response } = require("express");
-
-//LOGIN
-/*const reponseLogin = await fetch("http://localhost:5678/api/users/login");
-const login = await reponseLogin.json();
-console.log(login);
-const emailLogin = document.loginForm.email.value;
-const passwordLogin = document.loginForm.password.value;
-const loginBanner = document.querySelector(".login-banner");
-const btnLogin = document.querySelector(".connect");
-btnLogin.addEventListener("click", () => {
-  if (emailLogin === "p.a@gmail.com" && passwordLogin === "pa") {
-    window.location = "index.html";
-  }
+const btnFilterObject = document.querySelector("#btnFiltre1");
+btnFilterObject.addEventListener("click", function () {
+  const objectFilter = works.filter(function (work) {
+    return work.category.name == `object`;
+  });
+  document.querySelector(".galleryFigure").innerHTML = "";
+  genererWorks(objectFilter);
 });
-console.log(btnLogin);*/
+
+//MODAL GALERIE
+for (let z = 0; z < works.length; z++) {
+  const workModal = works[z];
+  console.log(workModal);
+  const sectionWorkModal = document.querySelector(".gallery-modal");
+  const elementWorkModal = document.createElement("figure");
+  //Id
+  const idWorkModal = elementWorkModal.setAttribute(
+    "id",
+    `${works[z].categoryId}`
+  );
+  console.log(idWorkModal);
+  //title
+  const imageWorkModal = document.createElement("img");
+  imageWorkModal.src = works[z].imageUrl;
+  imageWorkModal.alt = works[z].title;
+  //categoryId
+  //userId
+  //category: id + name
+  const editWorkModal = document.createElement("a");
+  editWorkModal.textContent = "éditer";
+  sectionWorkModal.appendChild(elementWorkModal);
+  elementWorkModal.appendChild(imageWorkModal);
+  elementWorkModal.appendChild(editWorkModal);
+}
+
+//MODAL GALERIE Création boutons add et remove
+const buttonWork = document.querySelector(".add-remove");
+const addButton = document.createElement("button");
+addButton.classList.add(`button-add`);
+addButton.textContent = "Ajouter une photo";
+buttonWork.appendChild(addButton);
+const removeButton = document.createElement("button");
+removeButton.classList.add(`button-remove`);
+removeButton.textContent = "supprimer la galerie";
+buttonWork.appendChild(removeButton);
